493625d1ecca90d720026dc0d9cc7e58145e372c
Aslan Inalov <aslaninalov@yandex.ru>
2023-07-27 23:34:05 +0300

    fixed1

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const burgerButton = document.querySelector('.burger-btn');
 const menu = document.querySelector('.burger-container');
 const closeButton = document.querySelector('.burger-container__close-btn');
+const body = document.querySelector('body');
+
 
 burgerButton.addEventListener('click', function () {
     menu.classList.add('burger-container_opened');
+    body.style.overflow = 'hidden';
 });
 
 closeButton.addEventListener('click', function () {
     menu.classList.remove('burger-container_opened');
-    menu.scrollTop = 0;
+    body.style.overflow = 'auto';
 });
 
 
@@ -16,6 +19,7 @@ const menuLinks = document.querySelectorAll('.burger-menu__link');
 menuLinks.forEach(function (link) {
     link.addEventListener('click', function () {
         menu.classList.remove('burger-container_opened');
+        body.style.overflow = 'auto';
     });
 });
 
